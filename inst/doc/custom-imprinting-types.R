5565becac32c697157a11cc0015673331c2ace78
kgostic <kategostic@gmail.com>
2022-08-26 16:22:40 -0700

    re-added vignettes after knitting

--- inst/doc/custom-imprinting-types.R
+++ inst/doc/custom-imprinting-types.R
@@ -1,18 +1,18 @@
-## ---- include = FALSE-------------------------------------
+## ---- include = FALSE----------------------------------------------------------------------------------
 knitr::opts_chunk$set(
 collapse = TRUE,
 comment = "#>"
 )
 library(dplyr)
 
-## ---------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------
 library(imprinting)
 ## Start with subtype-specific fractions for H1N1, H2N2, H3N2
 US_frequencies = get_country_cocirculation_data(country = 'United States', max_year = 2022) %>%
   select(1:4)
 head(US_frequencies)
 
-## ---------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------
 ## Add a vaccination column
 US_frequencies <- US_frequencies %>%
   mutate(vaccination = c(rep(0, 77), seq(.5, .75, length = 26), .75, .75), # Add a vaccination column
@@ -21,7 +21,7 @@ US_frequencies <- US_frequencies %>%
          `A/H3N2` = `A/H3N2`*(1-vaccination)) # fractions by one minus the year's vaccination probability.
 tail(US_frequencies, n = 30)
 
-## ---------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------
 Germany_frequencies <- get_country_cocirculation_data(country = 'Germany',
                                                       max_year = 2022) %>%
   select(1:4) %>%
@@ -31,12 +31,12 @@ Germany_frequencies <- get_country_cocirculation_data(country = 'Germany',
          `A/H3N2` = `A/H3N2`*(1-vaccination))
 tail(Germany_frequencies, 20)
 
-## ---------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------
 ## Check that all frequencies sum to 1
 rowSums(US_frequencies[,2:5])
 rowSums(Germany_frequencies[,2:5])
 
-## ---------------------------------------------------------
+## ------------------------------------------------------------------------------------------------------
 # Wrap the country-specific frequencies into a named list
 input_list = list("United States" = US_frequencies,
                   "Germany" = Germany_frequencies)
