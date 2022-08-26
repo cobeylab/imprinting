9a55e98ab68a8bb5349176c20e739d03bd78d3c0
kgostic <kategostic@gmail.com>
2022-08-26 15:23:34 -0700

    added updated vignette and documentation files

--- inst/doc/intro-to-imprinting.R
+++ inst/doc/intro-to-imprinting.R
@@ -1,83 +1,49 @@
-## ---- include = FALSE--------------------------------------------------------------------------------
+## ---- include = FALSE-------------------------------------
 knitr::opts_chunk$set(
 collapse = TRUE,
 comment = "#>"
 )
-
-## ----eval=FALSE--------------------------------------------------------------------------------------
-#  install.packages("devtools", repos='http://cran.us.r-project.org')
-
-## ----------------------------------------------------------------------------------------------------
-library(devtools) # Load the package
-
-## ----eval=FALSE--------------------------------------------------------------------------------------
-#  devtools::install_github("cobeylab/imprinting")
-
-## ----------------------------------------------------------------------------------------------------
-library(imprinting) 
-
-## ----include=FALSE-----------------------------------------------------------------------------------
 library(dplyr)
-library(ggplot2)
 
-## ----------------------------------------------------------------------------------------------------
-get_imprinting_probabilities(observation_years = 2022, countries = "United States")
+## ---------------------------------------------------------
+library(imprinting)
+## Start with subtype-specific fractions for H1N1, H2N2, H3N2
+US_frequencies = get_country_cocirculation_data(country = 'United States', max_year = 2022) %>%
+  select(1:4)
+head(US_frequencies)
 
-## ----------------------------------------------------------------------------------------------------
+## ---------------------------------------------------------
+## Add a vaccination column
+US_frequencies <- US_frequencies %>%
+  mutate(vaccination = c(rep(0, 77), seq(.5, .75, length = 26), .75, .75), # Add a vaccination column
+         `A/H1N1` = `A/H1N1`*(1-vaccination), # Assume only non-vaccinated children have primary
+         `A/H2N2` = `A/H2N2`*(1-vaccination), # infections; multiply the subtype-specific circulation
+         `A/H3N2` = `A/H3N2`*(1-vaccination)) # fractions by one minus the year's vaccination probability.
+tail(US_frequencies, n = 30)
+
+## ---------------------------------------------------------
+Germany_frequencies <- get_country_cocirculation_data(country = 'Germany',
+                                                      max_year = 2022) %>%
+  select(1:4) %>%
+  mutate(vaccination = c(rep(0, 87), seq(.05, .75, length = 16), .75, .75),
+         `A/H1N1` = `A/H1N1`*(1-vaccination), # Assume only non-vaccinated children have primary
+         `A/H2N2` = `A/H2N2`*(1-vaccination), # infections; multiply the subtype-specific circulation
+         `A/H3N2` = `A/H3N2`*(1-vaccination))
+tail(Germany_frequencies, 20)
+
+## ---------------------------------------------------------
+## Check that all frequencies sum to 1
+rowSums(US_frequencies[,2:5])
+rowSums(Germany_frequencies[,2:5])
+
+## ---------------------------------------------------------
+# Wrap the country-specific frequencies into a named list
+input_list = list("United States" = US_frequencies,
+                  "Germany" = Germany_frequencies)
+
+## Calculate probabilities
 get_imprinting_probabilities(observation_years = 2022, 
-                             countries = "United States", 
-                             df_format = 'wide')
-
-## ----------------------------------------------------------------------------------------------------
-get_imprinting_probabilities(observation_years = c(2005, 2011, 2012, 2022), 
-                             countries = "United States", 
-                             df_format = 'wide') %>%
-  filter(birth_year == 2000) %>%
-  mutate(age_at_observation = year-birth_year) %>%
-  select(c(1,2,3,8,4:7))
-
-## ----------------------------------------------------------------------------------------------------
-show_available_countries() %>%
-  print(n = 200)
-
-## ----------------------------------------------------------------------------------------------------
-many_probabilities = get_imprinting_probabilities(observation_years = c(2000, 2019, 2022),
-                                                  countries = c('Brazil', 'Afghanistan', 'Estonia', 'Finland')) 
-## Store the outputs in a variable called many_probabilities
-many_probabilities ## View the outputs in the console
-
-## ----eval=FALSE--------------------------------------------------------------------------------------
-#  # View the outputs in a separate window.
-#  View(many_probabilities)
-#  
-#  # Save the outputs as a .csv file in your current working directory.
-#  write_csv(many_probabilities, 'many_probabilities.csv')
-
-## ----fig.width = 7-----------------------------------------------------------------------------------
-head(many_probabilities)
-plot_one_country_year(many_probabilities) 
-
-## ----fig.width = 7-----------------------------------------------------------------------------------
-plot_one_country_year(many_probabilities %>% filter(country == 'Estonia', year == 2019))
-
-## ----fig.width = 7, fig.height = 5-------------------------------------------------------------------
-plot_many_country_years(many_probabilities)
-
-## ----------------------------------------------------------------------------------------------------
-get_country_cocirculation_data('United States', 2022)
-
-## ----------------------------------------------------------------------------------------------------
-get_country_intensity_data(country = 'China', max_year = 2022)
-
-## ----------------------------------------------------------------------------------------------------
-probs = get_p_infection_year(birth_year = 2000,
-                     observation_year = 2022,
-                     intensity_df = get_country_intensity_data('Mexico', 2022),
-                     max_year = 2022)
-names(probs) = as.character(2000+(0:12))
-probs
-sum(probs) ## Raw probabilities are not yet normalized.
-
-norm_probs = probs/sum(probs) ## Normalize
-sum(norm_probs)
+                             countries = c("United States", "Germany"), 
+                             annual_frequencies = input_list, 
+                             df_format = "wide")
 
